08eeee3b751fe9e94a926dc5809f7b4ab43e9717
Abu Nashir <abunashir@gmail.com>
2019-04-03 18:46:25 +0200

    Adopt thor gem for the metanorma cli
    The current version of metanorma cli is more likely a single
    ruby file, and we are doing all the option parsing and others
    manually. This also makes it bit harder to extend, and also
    forces us to re-write some common behavior by ourself.
    
    This commit changes this, and adopt the most commonly used
    library `thor` for this purpose, it also migrate all existing
    behavior to this new one
    
    Since `metanorma` is being used in production, and this new
    version is still in early stage, so instead of replacing it
    directly, we are doing a soft release. If you wanna give it
    a shot then you can do so by setting up an environment var
    `METANORMA_DEV_MODE` to `true`.

--- lib/metanorma/cli/command.rb
+++ lib/metanorma/cli/command.rb
@@ -1,4 +1,5 @@
 require "thor"
+require "metanorma/cli/compiler"
 require "metanorma/cli/generator"
 
 module Metanorma
@@ -10,7 +11,22 @@ module Metanorma
       option :overwrite, aliases: "-r", desc: "Overwrite existing document"
 
       def new(document_name)
-        Metanorma::Cli::Generator.run(document_name, options)
+        Metanorma::Cli::Generator.run(document_name, options.dup)
+      end
+
+      desc "compile FILENAME", "Compile to a metanorma document"
+      option :type, aliases: "-t", required: true, desc: "Type of standard to generate"
+      option :extensions, aliases: "-x", type: :array, desc: "Type of extension to generate per type"
+      option :format, aliases: "-f", default: :asciidoc, desc: "Format of source file: eg. asciidoc"
+      option :require, aliases: "-r", type: :array, desc: "Require LIBRARY prior to execution"
+      option :wrapper, aliases: "-w", type: :boolean, default: true, desc: "Create wrapper folder for HTML output"
+      option :asciimath, aliases: "-a", type: :boolean, default: true, desc: "Keep Asciimath in XML output instead of converting it to MathM"
+      option :datauriimage, aliases: "-d", type: :boolean, default: true, desc: "Encode HTML output images as data URIs"
+      option :relaton, aliases: "-R", desc: "Export Relaton XML for document to nominated filename"
+      option :extract, aliases: "-e", type: :array, desc: "Export sourcecode fragments from this document to nominated directory"
+
+      def compile(file_name)
+        Metanorma::Cli::Compiler.compile(file_name, options.dup)
       end
     end
   end
---
+++ lib/metanorma/cli/compiler.rb
@@ -0,0 +1,47 @@
+module Metanorma
+  module Cli
+    class Compiler
+      def initialize(file, options)
+        @file = file
+        @options = options
+        @extract = options.delete(:extract) || []
+        @extensions = options.delete(:extensions)
+      end
+
+      def compile
+        compile_file
+      end
+
+      def self.compile(file, options)
+        new(file, options).compile
+      end
+
+      private
+
+      attr_reader :file, :options, :extract, :extensions
+
+      def compile_file
+        Metanorma::Compile.new.compile(
+          file,
+          options.merge(customize_options),
+        )
+      end
+
+      def customize_options
+        extract_option.merge(extension_option)
+      end
+
+      def extract_option
+        Hash.new.tap do |hash|
+          hash[:extract] = extract[0]
+          hash[:extract_type] =
+            extract.size > 0 ? extract[0..-1].map(&:to_sym) : []
+        end
+      end
+
+      def extension_option
+        extensions ? { extension_keys: extensions.map(&:to_sym) } : {}
+      end
+    end
+  end
+end
