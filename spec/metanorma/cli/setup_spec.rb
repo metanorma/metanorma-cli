255dc8abc09dc4e056c611cb06845aeccff738da
Abu Nashir <abunashir@gmail.com>
2020-07-26 15:30:15 +0000

    Remove actual network call from test suite
    Context:
    
    Currently, the fontist related tests were performing actual network
    call for each request. Which is not ideal, and it is expected from
    the library to return paths of a fonts, not running the full cycle
    of downloading a fonts. That is already being tests in fontist.
    
    Solution:
    
    This commit removes this network calls, and instead of running the
    whole cycle it mocks the expected behavior, and only test what the
    metanorma cli is epxected to do. Since most of those fonts should
    not be distributable by third party so it also usages a fake font
    for the tests.
    
    Fixes 164

--- spec/metanorma/cli/setup_spec.rb
+++ spec/metanorma/cli/setup_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Metanorma::Cli::Setup do
   describe ".run" do
     context "with existing fonts" do
       it "copy overs the existing fonts" do
-        font_name = "Calibri"
+        font_name = "FakeCalibri"
 
-        stub_system_home_directory
+        allow(Metanorma::Cli).to receive(:fonts).and_return(fixture_fonts)
         Metanorma::Cli::Setup.run(font: font_name, term_agreement: true)
 
         expect(Metanorma::Cli.fonts.grep(/#{font_name}/i)).not_to be_nil
@@ -15,13 +15,19 @@ RSpec.describe Metanorma::Cli::Setup do
 
     context "with downloadale font" do
       it "downloads and copy over the font" do
-        font_name = "Calibri"
+        font_name = "FakeCalibri"
+
         stub_system_home_directory
 
+        allow(Fontist::Font).to receive(:install).and_return(fixture_fonts)
         Metanorma::Cli::Setup.run(font: "CALIBRI.TTF", term_agreement: true)
 
         expect(Metanorma::Cli.fonts.grep(/#{font_name}/i)).not_to be_nil
       end
     end
+  end
+
+  def fixture_fonts
+    [Metanorma::Cli.root_path.join("spec", "fixtures", "FakeCalibri.ttf").to_s]
   end
 end
