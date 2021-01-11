6d06810ec821c1d41ce226b5ce66fec130604201
Nick Nicholas <opoudjis@gmail.com>
2021-01-11 22:19:13 +1100

    ruby 2.4 debug

--- lib/metanorma/cli/command.rb
+++ lib/metanorma/cli/command.rb
@@ -67,7 +67,7 @@ module Metanorma
           opts = options.dup
           opts[:format] &&= opts[:format].split(",").map &:to_sym
           opts[:output_folder] = opts.delete :"output-folder"
-          opts[:compile] = opts.slice("agree-to-terms", "no-install-fonts", "continue-without-fonts").map { |k, v| [k.to_sym, v] }.to_h
+          opts[:compile] = opts.select { |k, v| ["agree-to-terms", "no-install-fonts", "continue-without-fonts"].include?(k)}.map { |k, v| [k.to_sym, v] }.to_h
           coll = Metanorma::Collection.parse filename
           coll.render opts
         else UI.say("Need to specify a file to process")
