79c4114a46b880c49a8be957e77ca324dfaf66bd
Alexande B <abobrikovich@gmail.com>
2021-02-27 17:37:27 +0300

    Fix undefined method 'global' for {global=>false}:Hash (NoMethodError) + don't crate empty config on 'get' subcommand

--- lib/metanorma/cli/thor_with_config.rb
+++ lib/metanorma/cli/thor_with_config.rb
@@ -7,15 +7,16 @@ module Metanorma
     class ThorWithConfig < Thor
       no_commands do
         def options
-          @options_cache
           original_options = super.to_hash.symbolize_all_keys
-          @options_cache = Metanorma::Cli::Commands::Config.load_configs(original_options)
+          Thor::CoreExt::HashWithIndifferentAccess.new(
+            Metanorma::Cli::Commands::Config.load_configs(original_options)
+          )
         end
 
         def filter_compile_options(options)
           options.select do |k, _|
-            %i[agree_to_terms no_install_fonts continue_without_fonts].include?(k)
-          end
+            %w[agree_to_terms no_install_fonts continue_without_fonts].include?(k)
+          end.symbolize_all_keys
         end
       end
     end
