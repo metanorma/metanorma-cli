46364284bff54859f67a03c6d9409caa97ef350a
Abu Nashir <abunashir@gmail.com>
2022-09-11 12:03:45 +0000

    Fix collection option related issue
    We have recently added support to provide collection options either
    as an command line arguments or options in the collection file. But
    that is only working on the site interface, and the collection cli
    still expect the `output-folder` as required option.
    
    This commit changes that, so it's not required anymore and it will
    try to read the option from the file, and if none of those source
    has this value, then it will use the source as output directory.
    
    Fixes #283

--- spec/metanorma/cli/collection_spec.rb
+++ spec/metanorma/cli/collection_spec.rb
@@ -5,9 +5,16 @@ RSpec.describe Metanorma::Cli::Collection do
     context "with specified options" do
       it "compiles and renders the collection files" do
         collection = mock_collection_instance
-        Metanorma::Cli::Collection.render(collection_file, format: "html, pdf")
 
-        expect(collection).to have_received(:render).with(format: %I(html pdf))
+        Metanorma::Cli::Collection.render(
+          collection_file,
+          format: "html, pdf",
+          output_folder: "bilingual-brochure",
+        )
+
+        expect(collection).to have_received(:render).with(
+          format: %I(html pdf), output_folder: "bilingual-brochure",
+        )
       end
     end
 
@@ -22,6 +29,18 @@ RSpec.describe Metanorma::Cli::Collection do
           coverpage: "collection_cover.html",
           output_folder: "bilingual-brochure",
           format: %I(xml html presentation pdf),
+        )
+      end
+    end
+
+    context "with missing options" do
+      it "usages the source as reference for options" do
+        collection = mock_collection_instance
+
+        Metanorma::Cli::Collection.render(collection_file)
+
+        expect(collection).to have_received(:render).with(
+          hash_including(output_folder: File.dirname(collection_file.to_s)),
         )
       end
     end
