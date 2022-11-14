e4e8f22b1454b58f4f57b96533fee7d290100125
Nick Nicholas <opoudjis@gmail.com>
2022-11-15 02:46:12 +1100

    remove rexml from rspec: https://github.com/metanorma/isodoc/issues/260

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,113 +52,109 @@ RSpec.configure do |config|
   config.include RSpecCommand
 end
 
-def xmlpp(x)
-  s = ""
-  f = REXML::Formatters::Pretty.new(2)
-  f.compact = true
-  f.write(REXML::Document.new(x),s)
-  s
+def xmlpp(xml)
+  Nokogiri::XML(xml).to_xml(indent: 2, encoding: "UTF-8")
 end
 
 ASCIIDOC_BLANK_HDR = <<~"HDR"
-      = Document title
-      Author
-      :docfile: test.adoc
-      :nodoc:
-      :novalid:
-      :no-isobib:
+  = Document title
+  Author
+  :docfile: test.adoc
+  :nodoc:
+  :novalid:
+  :no-isobib:
 
 HDR
 
 ASCIIDOC_PREAMBLE_HDR = <<~"HDR"
-      = Document title
-      Author
-      :docfile: test.adoc
-      :nodoc:
-      :novalid:
-      :no-isobib:
-      :mn-document-class: iso
-      :mn-output-extensions: xml,html
+  = Document title
+  Author
+  :docfile: test.adoc
+  :nodoc:
+  :novalid:
+  :no-isobib:
+  :mn-document-class: iso
+  :mn-output-extensions: xml,html
 
 HDR
 
 ASCIIDOC_CONFIGURED_HDR = <<~"HDR"
-      = Document title
-      Author
-      :docfile: test.adoc
-      :docnumber: 123
-      :nodoc:
-      :novalid:
-      :no-isobib:
-      :script: script.html
-      :body-font: body-font
-      :header-font: header-font
-      :monospace-font: monospace-font
-      :title-font: title-font
-      :i18nyaml: i18n.yaml
-      :stem:
+  = Document title
+  Author
+  :docfile: test.adoc
+  :docnumber: 123
+  :nodoc:
+  :novalid:
+  :no-isobib:
+  :script: script.html
+  :body-font: body-font
+  :header-font: header-font
+  :monospace-font: monospace-font
+  :title-font: title-font
+  :i18nyaml: i18n.yaml
+  :stem:
 
-      == Scope
-      image::spec/assets/rice_image1.png[]
+  == Scope
+  image::spec/assets/rice_image1.png[]
 
-      stem:[1/r]
+  stem:[1/r]
 
-      [source,ruby]
-      ----
-      def ruby(x)
-        if x < 0 && x > 1
-          return
-        end
-      end
-      ----
+  [source,ruby]
+  ----
+  def ruby(x)
+    if x < 0 && x > 1
+      return
+    end
+  end
+  ----
 HDR
 
 ISOXML_BLANK_HDR = <<~"HDR"
-<?xml version="1.0" encoding="UTF-8"?>
-<iso-standard xmlns="http://riboseinc.com/isoxml">
-<bibdata type="article">
-  <title>
-  </title>
-  <title>
-  </title>
-  <docidentifier>
-    <project-number>ISO </project-number>
-  </docidentifier>
-  <contributor>
-    <role type="author"/>
-    <organization>
-      <name>International Organization for Standardization</name>
-      <abbreviation>ISO</abbreviation>
-    </organization>
-  </contributor>
-  <contributor>
-    <role type="publisher"/>
-    <organization>
-      <name>International Organization for Standardization</name>
-      <abbreviation>ISO</abbreviation>
-    </organization>
-  </contributor>
-  <script>Latn</script>
-  <status>
-    <stage>60</stage>
-    <substage>60</substage>
-  </status>
-  <copyright>
-    <from>#{Time.new.year}</from>
-    <owner>
+  <?xml version="1.0" encoding="UTF-8"?>
+  <iso-standard xmlns="http://riboseinc.com/isoxml">
+  <bibdata type="article">
+    <title>
+    </title>
+    <title>
+    </title>
+    <docidentifier>
+      <project-number>ISO </project-number>
+    </docidentifier>
+    <contributor>
+      <role type="author"/>
       <organization>
         <name>International Organization for Standardization</name>
         <abbreviation>ISO</abbreviation>
       </organization>
-    </owner>
-  </copyright>
-  <editorialgroup>
-    <technical-committee/>
-    <subcommittee/>
-    <workgroup/>
-  </editorialgroup>
-</bibdata>
-</iso-standard>
+    </contributor>
+    <contributor>
+      <role type="publisher"/>
+      <organization>
+        <name>International Organization for Standardization</name>
+        <abbreviation>ISO</abbreviation>
+      </organization>
+    </contributor>
+    <script>Latn</script>
+    <status>
+      <stage>60</stage>
+      <substage>60</substage>
+    </status>
+    <copyright>
+      <from>#{Time.new.year}</from>
+      <owner>
+        <organization>
+          <name>International Organization for Standardization</name>
+          <abbreviation>ISO</abbreviation>
+        </organization>
+      </owner>
+    </copyright>
+    <editorialgroup>
+      <technical-committee/>
+      <subcommittee/>
+      <workgroup/>
+    </editorialgroup>
+  </bibdata>
+  </iso-standard>
 HDR
 
 def mock_pdf
