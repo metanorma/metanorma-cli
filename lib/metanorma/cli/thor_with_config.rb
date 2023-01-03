e382ece87c072a8a7d508fe0e15e9a13fc3c954b
Alexande B <abobrikovich@gmail.com>
2023-01-03 21:04:33 +0100

    metanorma/metanorma-docker#172 add strict mode for collection and siteget commands

--- lib/metanorma/cli/thor_with_config.rb
+++ lib/metanorma/cli/thor_with_config.rb
@@ -1,8 +1,6 @@
 require "thor"
 require "metanorma-utils"
 
-#require_relative "stringify_all_keys"
-
 module Metanorma
   module Cli
     class ThorWithConfig < Thor
@@ -13,14 +11,19 @@ module Metanorma
         def options
           original_options = super.to_hash.symbolize_all_keys
           Thor::CoreExt::HashWithIndifferentAccess.new(
-            Metanorma::Cli::Commands::Config.load_configs(original_options)
+            Metanorma::Cli::Commands::Config.load_configs(original_options),
           )
         end
 
         def filter_compile_options(options)
-          options.select do |k, _|
-            %w[agree_to_terms no_install_fonts continue_without_fonts no_progress].include?(k)
-          end.symbolize_all_keys
+          copts = %w[
+            agree_to_terms
+            no_install_fonts
+            continue_without_fonts
+            no_progress
+            strict
+          ]
+          options.select { |k, _| copts.include?(k) }.symbolize_all_keys
         end
       end
     end
--- spec/acceptance/generate_site_spec.rb
+++ spec/acceptance/generate_site_spec.rb
@@ -33,23 +33,35 @@ RSpec.describe "Metanorma" do
       output_dir = Dir.pwd
       allow(Metanorma::Cli::SiteGenerator).to receive(:generate).and_call_original
       allow(Metanorma::Cli::Compiler).to receive(:compile)
-      command = %W(site generate #{source_dir} -o #{output_dir} --continue-without-fonts)
+      command = %W(site generate #{source_dir} -o #{output_dir}
+                   --continue-without-fonts -S)
 
       output = capture_stdout { Metanorma::Cli.start(command) }
 
       expect(output).to include("Site has been generated at #{output_dir}")
       expect(Metanorma::Cli::SiteGenerator).to have_received(:generate).with(
         source_dir.to_s,
-        { output_dir: output_dir, no_progress: true, continue_without_fonts: true },
+        {
+          output_dir: output_dir,
+          no_progress: true,
+          continue_without_fonts: true,
+          strict: true,
+        },
         continue_without_fonts: true,
-        no_progress: true
+        no_progress: true,
+        strict: true,
       )
 
       expect(Metanorma::Cli::Compiler).to have_received(:compile)
         .at_least(:once)
         .with(
           kind_of(String),
-          hash_including(format: :asciidoc, output_dir: kind_of(Pathname), continue_without_fonts: true)
+          hash_including(
+            format: :asciidoc,
+            output_dir: kind_of(Pathname),
+            continue_without_fonts: true,
+            strict: true,
+          ),
         )
     end
 
@@ -79,7 +91,7 @@ RSpec.describe "Metanorma" do
       expect(Metanorma::Cli::SiteGenerator).to have_received(:generate).with(
         source_dir.to_s,
         hash_including(template_dir: template_dir, stylesheet: stylesheet_path),
-        no_progress: true
+        no_progress: true,
       )
     end
   end
