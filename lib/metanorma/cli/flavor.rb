904a1d398438c28cfa52a9d73593ac7314c43e80
Nick Nicholas <opoudjis@gmail.com>
2026-03-07 18:04:17 +1100

    report metanorma-taste in metanorma version: https://github.com/metanorma/metanorma-taste/issues/114

--- lib/metanorma/cli/flavor.rb
+++ lib/metanorma/cli/flavor.rb
@@ -106,8 +106,8 @@ module Metanorma
 
       DEPENDENCY_GEMS =
         %w(html2doc isodoc metanorma-utils mn2pdf mn-requirements isodoc-i18n
-           metanorma-plugin-glossarist
-           metanorma-plugin-lutaml relaton-cli pubid glossarist fontist
+           metanorma-plugin-glossarist metanorma-plugin-lutaml 
+           metanorma-taste relaton-cli pubid glossarist fontist
            plurimath lutaml expressir xmi lutaml-model emf2svg unitsml
            vectory ogc-gml oscal).freeze
 
